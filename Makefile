bb4361033f3aff24f159c7c88c8fb9a690717386
Dusty Mabe <dusty@dustymabe.com>
2019-10-24 12:38:07 -0400

    simple-procfs-kmod is working now!
    ```
    $ sudo cat /proc/simple-procfs-kmod
    simple-procfs-kmod number = 0
    $
    $ sudo bash -c "echo 40 > /proc/simple-procfs-kmod"
    $
    $ sudo cat /proc/simple-procfs-kmod
    simple-procfs-kmod number = 40
    ```

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 obj-m += simple-kmod.o
+obj-m += simple-procfs-kmod.o
 
 ifndef KVER
 KVER=$(shell uname -r)
@@ -16,5 +17,6 @@ clean:
 	make -C /lib/modules/$(KVER)/build M=$(PWD) clean
 install:
 	sudo install -v -m 755 -d /lib/modules/$(KVER)/
-	sudo install -v -m 644 simple-kmod.ko /lib/modules/$(KVER)/simple-kmod.ko
+	sudo install -v -m 644 simple-kmod.ko        /lib/modules/$(KVER)/simple-kmod.ko
+	sudo install -v -m 644 simple-procfs-kmod.ko /lib/modules/$(KVER)/simple-procfs-kmod.ko
 	sudo depmod -a
--- simple-procfs-kmod.c
+++ simple-procfs-kmod.c
@@ -1,3 +1,8 @@
+/**
+ *  simple-procfs-kmod.c -  Simple kmod that creates a "file" in /proc
+ *
+ */
+
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
@@ -6,31 +11,36 @@
 #include <asm/uaccess.h>
 #define BUFSIZE  100
 
+#define PROCFS_NAME "simple-procfs-kmod"
 
+// Set module version 
+MODULE_VERSION(KMODVER);
+
+// Code taken from https://github.com/dev-area/proctest
+// and modified slightly to use the `simple-procfs-kmod` file
+// under the proc filesystem.
+//
+// Original attribution to author here:
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Liran B.H");
 
-static int irq=20;
-module_param(irq,int,0660);
-
-static int mode=1;
-module_param(mode,int,0660);
+static int number=0;
+module_param(number,int,0660);
 
 static struct proc_dir_entry *ent;
 
 static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos) 
 {
-    int num,c,i,m;
+    int num,c,i;
     char buf[BUFSIZE];
     if(*ppos > 0 || count > BUFSIZE)
         return -EFAULT;
-    if(copy_from_user(buf,ubuf,count))
+    if(raw_copy_from_user(buf,ubuf,count))
         return -EFAULT;
-    num = sscanf(buf,"%d %d",&i,&m);
-    if(num != 2)
+    num = sscanf(buf,"%d",&i);
+    if(num != 1)
         return -EFAULT;
-    irq = i; 
-    mode = m;
+    number = i;
     c = strlen(buf);
     *ppos = c;
     return c;
@@ -42,10 +52,9 @@ static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t 
     int len=0;
     if(*ppos > 0 || count < BUFSIZE)
         return 0;
-    len += sprintf(buf,"irq = %d\n",irq);
-    len += sprintf(buf + len,"mode = %d\n",mode);
+    len += sprintf(buf,"%s number = %d\n",PROCFS_NAME, number);
     
-    if(copy_to_user(ubuf,buf,len))
+    if(raw_copy_to_user(ubuf,buf,len))
         return -EFAULT;
     *ppos = len;
     return len;
@@ -60,15 +69,15 @@ static struct file_operations myops =
 
 static int simple_init(void)
 {
-    ent=proc_create("mydev",0660,NULL,&myops);
-    printk(KERN_ALERT "hello...\n");
+    ent=proc_create(PROCFS_NAME,0660,NULL,&myops);
+    printk(KERN_INFO "%s: hello ...\n", PROCFS_NAME);
     return 0;
 }
 
 static void simple_cleanup(void)
 {
     proc_remove(ent);
-    printk(KERN_WARNING "bye ...\n");
+    printk(KERN_INFO "%s: bye ...\n", PROCFS_NAME);
 }
 
 module_init(simple_init);
