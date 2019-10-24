9a05272fec92c56dcafb3daf447cf95a84a981b1
Dusty Mabe <dusty@dustymabe.com>
2019-10-24 12:44:41 -0400

    simple-procfs-kmod-userspace-tool: it works!
    ```
    $ sudo /usr/bin/spkut 44
    simple-procfs-kmod number = 0
    
    simple-procfs-kmod number = 44
    ```

--- Makefile
+++ Makefile
@@ -13,9 +13,12 @@ buildprep:
 	sudo yum install -y gcc kernel-{core,devel,modules}-$(KVER)
 all:
 	make -C /lib/modules/$(KVER)/build M=$(PWD) EXTRA_CFLAGS=-DKMODVER=\\\"$(KMODVER)\\\" modules
+	gcc -o spkut ./simple-procfs-kmod-userspace-tool.c
 clean:
 	make -C /lib/modules/$(KVER)/build M=$(PWD) clean
+	rm -f spkut
 install:
+	sudo install -v -m 755 spkut /bin/
 	sudo install -v -m 755 -d /lib/modules/$(KVER)/
 	sudo install -v -m 644 simple-kmod.ko        /lib/modules/$(KVER)/simple-kmod.ko
 	sudo install -v -m 644 simple-procfs-kmod.ko /lib/modules/$(KVER)/simple-procfs-kmod.ko
--- simple-procfs-kmod-userspace-tool.c
+++ simple-procfs-kmod-userspace-tool.c
@@ -1,20 +1,31 @@
+/**
+ *  simple-procfs-kmod-userspace-tool.c -  Simple tool that reads/writes proc file
+ *
+ */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-void main(void)
+// Code taken from https://github.com/dev-area/proctest
+// and modified slightly to use the `simple-procfs-kmod` file
+// under the proc filesystem.
+
+#define PROCFS_FILE_PATH "/proc/simple-procfs-kmod"
+
+void main(int argc, char *argv[])
 {
-    char buf[100];
-    int fd = open("/proc/mydev", O_RDWR);
-    read(fd, buf, 100);
-    puts(buf);
+	char buf[100];
+	int fd = open(PROCFS_FILE_PATH, O_RDWR);
+	read(fd, buf, 100);
+	puts(buf);
 
-    lseek(fd, 0 , SEEK_SET);
-    write(fd, "33 4", 5);
-
-    lseek(fd, 0 , SEEK_SET);
-    read(fd, buf, 100);
-    puts(buf);
-}
+	lseek(fd, 0 , SEEK_SET);
+	write(fd, argv[1], 100);
+	
+	lseek(fd, 0 , SEEK_SET);
+	read(fd, buf, 100);
+	puts(buf);
+}	
